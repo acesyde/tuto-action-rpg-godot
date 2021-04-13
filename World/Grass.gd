669f24587f6b17f9b66fe1f4065f5d5fa28d4d5b
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2021-04-13 22:36:14 +0200

    pt10

---
+++ World/Grass.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		var grassEffectScene = load("res://Effects/GrassEffect.tscn")
+		var grassEffect = grassEffectScene.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
+		queue_free()
