31d05e05928b8e24bac3a857d42314e792a928bb
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2021-04-14 22:17:30 +0200

    pt11/pt12

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grassEffectScene = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = grassEffectScene.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var grassEffectScene = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = grassEffectScene.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free() # Replace with function body.
